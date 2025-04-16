cd7fa691ffb4dc994dfde0a4c6fb17785e84a41e
preetimishra28@gmail.com <preeti.mishra@softwire.com>
2025-04-16 15:41:47 +0100

    Add date

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,19 @@
-
+import {useState} from "react";
 import "../styles/styles.scss"
 
 export const Header =()=>{
+
+    // const [time,setTime] = useState(new Date().toLocaleTimeString());
+    // setInterval(()=>setTime(new Date().toLocaleTimeString()));
+
+
+    const [time,setTime] = useState(new Date().toLocaleString());
+    setInterval(()=>setTime(new Date().toLocaleString()));
+
     return(
         <div>
-            <h1 className="header">Keeper</h1>
+            <h1 className="header">Keeper  {time}</h1>
+            {/* <h2 className="header h2"> </h2> */}
         </div>
     )
 }
