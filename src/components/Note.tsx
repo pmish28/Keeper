4a9a52e355d12bfaae4c686b5730a611d3197fca
preetimishra28@gmail.com <preeti.mishra@softwire.com>
2025-04-16 16:01:48 +0100

    Implmented mouse over and mouse leave on note

--- src/components/Note.tsx
+++ src/components/Note.tsx
@@ -1,5 +1,14 @@
+import { useState } from "react"
 import {NoteItem} from "../interfaces"
 
 export const Note =(props :NoteItem)=>{
-    return(<div className="note"><h1>{props.title}</h1><p>{props.content}</p></div>)
+
+    const [isMouseOver, setMouseOver] = useState(false);
+    const HandleMouseOver =()=>{
+        setMouseOver(true);
+    }
+    const HandleMouseLeave =()=>{
+        setMouseOver(false);
+    }
+    return(<div onMouseOver={HandleMouseOver} onMouseLeave={HandleMouseLeave} className={isMouseOver? "noteHighlight" : "note"}><h1>{props.title}</h1><p>{props.content}</p></div>)
 }
